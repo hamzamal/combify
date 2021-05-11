2872e460b8e75ec2d57e0d788422dc2b7f1b86d7
Hamza Malik <muhammadhamza.malik@alumnos.upm.es>
2021-05-11 19:25:47 +0500

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -63,4 +63,9 @@ model.fit(X_train,
                        verbose=1)]
            )
 
-joblib.dump(model, "rf_model.sav")
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
